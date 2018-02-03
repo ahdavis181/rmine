1da05b56b5d602f667627cbef7fa3e323cf1be6d
Adam H. Davis <ahdavis181@protonmail.com>
2018-02-03 22:56:07 +0000

    adding parsing of CSV into searchTerms objects

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-var keypairs = require('ripple-keypairs');
+const keypairs = require('ripple-keypairs');
+const parse = require('csv-parse/lib/sync');
+const fs = require('fs');
 
 exports.testFunction = function() {
 	var seed = (process.argv.length==3) ? process.argv[2] : keypairs.generateSeed();
 	console.log("Testing seed generator: " + seed);
+}
+
+exports.searchTermsFromCSV = function(path) {
+	var CSVFileContents = fs.readFileSync(path).toString();
+	console.log(CSVFileContents);
+	var searchTerms = [];
+	var searchTermDataRows = parse(CSVFileContents, {auto_parse: true}).forEach(function(searchTermDataRow) {
+		searchTerms.push(new SearchTerm(searchTermDataRow));
+	});
+	searchTerms.splice(0,1);
+	console.log(searchTerms);
+	return searchTerms;
+}
+
+function SearchTerm (searchTermData) {
+	this.searchString = searchTermData[0];
+	this.caseSensitive = Boolean(searchTermData[1]);
+	this.searchIndex = parseInt(searchTermData[2]);
 }
